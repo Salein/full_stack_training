416304034e9f28db469cc27879b6760ffe9f4aac
Sallein <saleinkyl@gmail.com>
2024-04-17 21:26:36 +0300

    создал роуты и проверил работоспособность через постман

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 require("dotenv").config()
 
 const express = require("express")
-const sequelize = require('./db')
-const models = require('./models/models')
-const cors = require('cors')
+const sequelize = require("./db")
+const models = require("./models/models")
+const cors = require("cors")
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 8000
 
@@ -11,15 +12,13 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'working'})
-})
 
 const start = async () => {
     try {
-       await sequelize.authenticate()
-       await sequelize.sync()
+        await sequelize.authenticate()
+        await sequelize.sync()
         app.listen(PORT, () => console.log(`SERVER STARTED ON PORT: ${PORT}`))
     } catch (e) {
         console.log(e)
