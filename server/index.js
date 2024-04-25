f0c6fc718c39369324fe820f2e1b43f850ef802b
Sallein <saleinkyl@gmail.com>
2024-04-25 18:33:10 +0300

    создан метод create в deviceController

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require("express")
 const sequelize = require("./db")
 const models = require("./models/models")
 const cors = require("cors")
+const fileupload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 8000
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(fileupload({}))
 app.use('/api', router)
 app.use(errorHandler)
 
