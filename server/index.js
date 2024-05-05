e306828d91e856df28a4b8f3306f479d8edede6e
Sallein <saleinkyl@gmail.com>
2024-05-05 14:25:54 +0300

    доработан deviceController но крашится с поиском по id (баг)

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const cors = require("cors")
 const fileupload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 
 const PORT = process.env.PORT || 8000
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 8000
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileupload({}))
 app.use('/api', router)
 app.use(errorHandler)
