d0bdc09ff7a66e767398b0a03651d31c0191af00
Sallein <saleinkyl@gmail.com>
2024-06-30 19:37:43 +0300

    создана структура и описаны маршруты

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
-
+import React from "react"
+import { BrowserRouter } from "react-router-dom"
+import AppRouter from "./components/AppRouter"
 
 const App = () => {
-  return (
-    <div>
-      hawdawd
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
