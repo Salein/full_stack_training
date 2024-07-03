546d3cc393295929e57ab14f91f0c4445141ed65
Sallein <saleinkyl@gmail.com>
2024-07-04 01:09:33 +0300

    создал роуты но нужно пофиксить

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Switch, Route, Redirect} from 'react-router-dom'
+import {Route, Routes, useLocation} from 'react-router-dom'
+import { authRoutes, publicRoutes } from '../routes';
 
 const AppRouter = () => {
+    const isAuth = false
+    const url = useLocation()
+    const path = url.pathname
     return (
-        <div>
-            
-        </div>
+        
+        <Routes>
+            {isAuth && authRoutes.map(({path, Component}) => 
+            <Route key={path} path={path} element={<Component/>} exact />)}
+            {publicRoutes.map(({path, Component}) => 
+            <Route key={path} path={path} element={<Component/>} exact />)}
+        </Routes>
     );
 };
 
