f64ae98aa14a1b7b19596d891bfe2964a1a1cc09
Sallein <saleinkyl@gmail.com>
2024-07-04 23:08:59 +0300

    починил роуты

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import {Route, Routes, useLocation} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import { authRoutes, publicRoutes } from '../routes';
 
 const AppRouter = () => {
     const isAuth = false
-    const url = useLocation()
-    const path = url.pathname
     return (
         
         <Routes>
             {isAuth && authRoutes.map(({path, Component}) => 
-            <Route key={path} path={path} element={<Component/>} exact />)}
+            <Route key={path} path={path} element={<Component/>} />)}
             {publicRoutes.map(({path, Component}) => 
-            <Route key={path} path={path} element={<Component/>} exact />)}
+            <Route key={path} path={path} element={<Component/>} />)}
         </Routes>
     );
 };
