4fcbfb36e36e562b14fb8acec7cb39b041bd3647
Sallein <saleinkyl@gmail.com>
2024-07-25 21:40:12 +0300

    баг в DeviceItem: сползла верстка

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -2,19 +2,23 @@ import React from 'react';
 import Card from 'react-bootstrap/esm/Card';
 import Col from 'react-bootstrap/esm/Col';
 import Image from 'react-bootstrap/Image'
+import star from '../assets/star.png'
 
 const DeviceItem = ({device}) => {
     return (
         <Col md={3}>
             <Card
+            className='p-1'
             style={{width: 150, cursor: 'pointer'}}
-            border={'red'}
+            border={'green'}
             >
-                <Image width={150} height={150} src={device.img} />
-                <div>
+                <Image width={130} height={150} src={device.img}
+                className='p-2'/>
+                <div className='d-flex justify-content-between align-items-center '>
                     <div>Samsung</div>
-                    <div>
-                        <div>{device.rating}</div>
+                    <div className='d-flex justify-content-end m-1'>
+                        <div>5</div>
+                        <Image src={star}  />
                     </div>
                 </div>
             </Card>
