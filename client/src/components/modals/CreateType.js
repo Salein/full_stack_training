f9dbe109b071c398cf16dbb8e11519c4b76a316c
Sallein <saleinkyl@gmail.com>
2024-08-01 19:40:02 +0300

    почти готовы 3 модалки осталось причесать

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react"
+import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/esm/Button"
+import Form from "react-bootstrap/Form"
 
-const CreateType = () => {
+const CreateType = ({ show, onHide }) => {
     return (
-        <div>
-            
-        </div>
-    );
-};
+        <Modal show={show} onHide={onHide} size="lg" centered>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Добавить тип
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Control placeholder="Введите название типа" />
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={onHide}>
+                    Закрыть
+                </Button>
+                <Button variant="outline-success" onClick={onHide}>
+                    Добавить
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
 
-export default CreateType;
+export default CreateType
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,8 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import Button from "react-bootstrap/esm/Button"
 import Container from "react-bootstrap/esm/Container"
+import CreateBrand from "../components/modals/CreateBrand"
+import CreateDevice from "../components/modals/CreateDevice"
+import CreateType from "../components/modals/CreateType"
 
 const Admin = () => {
+    const [brandVisible, setBrandVisible] = useState(false)
+    const [typeVisible, setTypeVisible] = useState(false)
+    const [deviceVisible, setDeviceVisible] = useState(false)
+
     return (
         <Container className="d-flex flex-column">
             <Button variant="outline-dark" className="mt-4 p-2">
@@ -14,6 +21,9 @@ const Admin = () => {
             <Button variant="outline-dark" className="mt-4 p-2">
                 Добавить устройство
             </Button>
+            <CreateBrand show={brandVisible}/>
+            <CreateDevice show={deviceVisible}/>
+            <CreateType show={typeVisible}/>
         </Container>
     )
 }
