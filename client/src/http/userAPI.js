7be1a6c301da1c2d325c76f0e61c27d58843216b
Sallein <saleinkyl@gmail.com>
2024-08-07 22:27:35 +0300

    установлена библиотека jwt-decode и применена для удобочитаемого формата в логах и в целом

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -1,17 +1,18 @@
 import { $authHost, $host } from "./index"
+import {jwtDecode} from 'jwt-decode'
 
 export const registration = async (email, password) => {
-    const response = await $host.post("api/user/registration", {
+    const {data} = await $host.post("api/user/registration", {
         email,
         password,
         role: "ADMIN",
     })
-    return response
+    return jwtDecode(data.token)
 }
 
 export const login = async (email, password) => {
-    const response = await $host.post("api/user/login", { email, password })
-    return response
+    const {data} = await $host.post("api/user/login", { email, password })
+    return jwtDecode(data.token)
 }
 
 export const check = async () => {
